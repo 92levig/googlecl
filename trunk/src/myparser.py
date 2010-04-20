ff22acd988f79a2e4c82ab90adb82a2945327b81
tom.h.miller <tom.h.miller@03bb9ad7-192f-422b-88de-2d13e6ea63b0>
2010-04-20 15:13:50 +0000

    Bugfixes for MyParser. Non-quoted final arguments are included, and arguments lacking any quotes are ok.

--- trunk/src/myparser.py
+++ trunk/src/myparser.py
@@ -16,29 +16,39 @@ class MyParser(optparse.OptionParser):
     def parse_args(self, given_args=sys.argv[1:]):
         """Parse arguments with quotes.
         
-        Same as optparse.OptionParser.parse_args(), but combines args in double
-        quotes. For example, picasa delete -a "Boring Album Title" will
-        combine "Boring Album Title" into the argument for -a 
+        Same as optparse.OptionParser.parse_args(), but combines args in quotes.
+        For example, picasa delete -a "Boring Album Title" will combine 
+        "Boring Album Title" into the argument for -a 
+        
+        Keyword arguments:
+        given_args -- the list of arguments. (default sys.argv[1:])
+        
+        Returns: optparse.OptionParser.parse_args(self, merged_arguments_list)
         
         """
         if not given_args:
             return optparse.OptionParser.parse_args(self, given_args)
         merged_args = ' '.join(given_args)
         quote_index = merged_args.find('"')
-        final_args = []
-        while quote_index != -1:
-            start = quote_index
-            end = merged_args[start+1:].find('"') + start + 1
-            quoted_arg = merged_args[start+1:end] 
-            non_quoted_args = merged_args[:start].split()
-            final_args.extend(non_quoted_args)
-            final_args.extend([quoted_arg])
-            merged_args = merged_args[end+1:]
-            if merged_args:
-                quote_index = merged_args.find('"')
-            else:
-                quote_index = -1
-            
+        if quote_index == -1:
+            final_args = given_args
+        else:
+            final_args = []
+            while quote_index != -1:
+                start = quote_index
+                end = merged_args[start+1:].find('"') + start + 1
+                quoted_arg = merged_args[start+1:end] 
+                non_quoted_args = merged_args[:start].split()
+                final_args.extend(non_quoted_args)
+                final_args.extend([quoted_arg])
+                merged_args = merged_args[end+1:]
+                if merged_args:
+                    quote_index = merged_args.find('"')
+                else:
+                    quote_index = -1
+                    
+            final_args.extend([merged_args.strip()])
+        
         return optparse.OptionParser.parse_args(self, final_args)
             
                 
