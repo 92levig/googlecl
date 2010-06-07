53b276208d2240f8ae1029218ae32025992fd746
tom.h.miller <tom.h.miller@03bb9ad7-192f-422b-88de-2d13e6ea63b0>
2010-06-07 15:26:32 +0000

    Moved old docs/service.py to docs/client.py. Made docs/service.py compatible with gdata-python-client 1.2.4.

--- trunk/src/googlecl/docs/client.py
+++ trunk/src/googlecl/docs/client.py
@@ -1,5 +1,12 @@
 """
 Service details and instances for the Docs service.
+
+COMPATABILTIY NOTE: This is one of the few Python modules that uses the 2.0
+gdata API, and is still in labs. It's likely that this will break often.
+The code in this file tries to follow the style found in gdata.client and
+gdata.docs.client, instead of the rest of the GoogleCL project. The idea is
+that the other services/clients will also be updated to look like the 2.0
+compatable code in the near future.
  
 Some use cases:
 Upload a document:
@@ -18,24 +25,17 @@ Created on May 13, 2010
 """
 __author__ = 'tom.h.miller@gmail.com (Tom Miller)'
 import ConfigParser
-import gdata.docs.service
+import gdata.docs.client
 import re
 import os
-from googlecl import util
+import urllib
+import util
+from gdata.client import BadAuthentication, CaptchaChallenge
 from googlecl.docs import SECTION_HEADER
 
-
-# These definitions are from gdata.docs.data in higher versions of gdata.
-DOCUMENT_LABEL = 'document'
-SPREADSHEET_LABEL = 'spreadsheet'
-PRESENTATION_LABEL = 'presentation'
-FOLDER_LABEL = 'folder'
-PDF_LABEL = 'pdf'
-
-
-class DocsServiceCL(gdata.docs.service.DocsService, util.BaseServiceCL):
+class DocsClientCL(gdata.docs.client.DocsClient):
   
-  """Extends gdata.docs.service.DocsClient for the command line.
+  """Extends gdata.docs.client.DocsClient for the command line.
   
   This class adds some features focused on using Google Docs via an installed
   app with a command line interface.
@@ -54,8 +54,39 @@ class DocsServiceCL(gdata.docs.service.DocsService, util.BaseServiceCL):
                      deleting a document. (Default True)
               
     """ 
-    gdata.docs.service.DocsService.__init__(self, source='GoogleCL')
-    util.BaseServiceCL.set_params(self, regex, tags_prompt, delete_prompt)
+    gdata.docs.client.DocsClient.__init__(self, source='GoogleCL')
+    self.logged_in = False
+    self.use_regex = regex
+    self.prompt_for_tags = tags_prompt
+    self.prompt_for_delete = delete_prompt
+  
+  def delete(self, entries, entry_type, delete_default):
+    """Extends Delete to handle a list of entries.
+    
+    Keyword arguments:
+      entries: List of entries to delete.
+      entry_type: String describing the thing being deleted (e.g. album, post).
+      delete_default: Whether or not the default action should be deletion.
+      
+    """
+    if delete_default and self.prompt_for_delete:
+      prompt_str = '(Y/n)'
+    elif self.prompt_for_delete:
+      prompt_str = '(y/N)'
+    for item in entries:
+      if self.prompt_for_delete:
+        delete_str = raw_input('Are you SURE you want to delete %s "%s"? %s: ' % 
+                               (entry_type, item.title.text, prompt_str))
+        if not delete_str:
+          delete = delete_default
+        else:
+          delete = delete_str.lower() == 'y'
+      else:
+        delete = True
+      if delete:
+        gdata.docs.client.DocsClient.Delete(self, item)
+
+  Delete = delete
 
   def edit_doc(self, doc_entry, editor, format):
     """Edit a document.
@@ -70,11 +101,12 @@ class DocsServiceCL(gdata.docs.service.DocsService, util.BaseServiceCL):
     import subprocess
     import tempfile
     import shutil
-    from gdata.docs.service import SUPPORTED_FILETYPES
+    from gdata.docs.data import MIMETYPES
+    from gdata.data import MediaSource
     
     temp_dir = tempfile.mkdtemp()
     path = os.path.join(temp_dir, doc_entry.title.text + '.' + format)
-    self.Export(doc_entry, path)
+    self.export(doc_entry, path)
     create_time = os.stat(path).st_mtime
     subprocess.call([editor, path])
     if create_time == os.stat(path).st_mtime:
@@ -82,14 +114,14 @@ class DocsServiceCL(gdata.docs.service.DocsService, util.BaseServiceCL):
       return
     else:
       try:
-        content_type = SUPPORTED_FILETYPES[format.upper()]
+        content_type = MIMETYPES[format.upper()]
       except KeyError:
         print 'Could not find mimetype for ' + format
-        while format not in SUPPORTED_FILETYPES.keys():
-          format = raw_input('Please enter one of ' + SUPPORTED_FILETYPES.keys() + 
+        while format not in MIMETYPES.keys():
+          format = raw_input('Please enter one of ' + MIMETYPES.keys() + 
                              ' for a content type to upload as.')
-        content_type = SUPPORTED_FILETYPES[format]
-      mediasource = gdata.MediaSource(file_path=path, content_type=content_type)
+        content_type = MIMETYPES[format]
+      mediasource = MediaSource(file_path=path, content_type=content_type)
       self.Update(doc_entry, media_source=mediasource)
     try:
       # Good faith effort to keep the temp directory clean.
@@ -112,11 +144,11 @@ class DocsServiceCL(gdata.docs.service.DocsService, util.BaseServiceCL):
     
     """
     for entry in entries:
-      format = get_extension(get_document_type(entry))
+      format = get_extension(entry.GetDocumentType())
       path = os.path.join(base_path, entry.title.text + '.' + format)
       print 'Downloading ' + entry.title.text + ' to ' + path
       try:
-        self.Export(entry, path)
+        self.export(entry, path)
       except Exception, e:
         print e
         print 'Download of ' + entry.title.text + ' failed'
@@ -139,15 +171,15 @@ class DocsServiceCL(gdata.docs.service.DocsService, util.BaseServiceCL):
       
     """
     if folder:
-      q = gdata.docs.service.DocumentQuery(categories=['folder'],
-                                           params={'showfolders': 'true'})
-      feed = self.Query(q.ToUri())
+      feed = gdata.docs.client.DocsClient.get_doclist(self,
+                                    uri='/feeds/default/private/full/-/folder')
       entries = []
       for f in feed.entry:
         # Skip folders that do not match the name we're looking for
         if ((self.use_regex and re.match(folder,f.title.text)) or
             (not self.use_regex and folder == f.title.text)):
-          contents = self.QueryDocumentListFeed(uri=f.content.src)
+          contents = gdata.docs.client.DocsClient.get_doclist(self,
+                                                              uri=f.content.src)
           if not title:
             entries.extend(contents.entry)
           elif self.use_regex:
@@ -156,11 +188,16 @@ class DocsServiceCL(gdata.docs.service.DocsService, util.BaseServiceCL):
           else:
             entries.extend([entry for entry in contents.entry
                             if title == entry.title.text])
-    else:
-      q = gdata.docs.service.DocumentQuery()
-      entries = self.GetEntries(q.ToUri(),
-                                title,
-                                converter=gdata.docs.DocumentListFeedFromString)
+    else: 
+      f = gdata.docs.client.DocsClient.get_doclist(self)
+      if not title:
+        return f.entry
+      if self.use_regex:
+        entries = [entry for entry in f.entry
+                   if re.match(title,entry.title.text)]
+      else:
+        entries = [entry for entry in f.entry
+                   if title == entry.title.text]
     return entries
 
   def get_single_doc(self, title=None, folder=None):
@@ -196,8 +233,7 @@ class DocsServiceCL(gdata.docs.service.DocsService, util.BaseServiceCL):
   def is_token_valid(self):
     """Check that the token being used is valid."""
     try:
-      q = gdata.docs.service.DocumentQuery()
-      self.Get(q.ToUri())
+      self.Get('/feeds/default/private/full')
     except gdata.service.RequestError, e:
       if e.args[0]['body'].find('Token invalid') != -1:
         return False
@@ -208,40 +244,82 @@ class DocsServiceCL(gdata.docs.service.DocsService, util.BaseServiceCL):
   
   IsTokenValid = is_token_valid
 
-  def upload_docs(self, paths, title=None):
+  def login(self, email, password):
+    """Log in to the docs service.
+    
+    Keyword arguments:
+      email: Email account to use.
+      password: Password associated with said account.
+    
+    Returns:
+      Nothing, but sets self.logged_in to true on success.
+    
+    """
+    self.logged_in = False
+    if not (email and password):
+      print ('You must give an email/password combo to log in with.')
+      return
+    
+    try:
+      self.client_login(email, password, 'GoogleCL')
+    except BadAuthentication, e:
+      print e
+    except CaptchaChallenge:
+      print 'Too many failed logins; Captcha required.'
+    else:
+      self.logged_in = True
+
+  Login = login
+
+  def upload_docs(self, paths, title=None, folder=None, convert=True):
     """Upload a document.
     
     Keyword arguments:
       paths: Paths of files to upload.
       title: Title to give the files once uploaded.
              (Default None for the names of the files).
+      folder: Folder to put the files in. (Default None for the root folder).
+      convert: If True, converts the files to the native Google Docs format.
+               Otherwise, leaves as arbitrary file type. Only Google Apps
+               Premier users can specify a value other than True. (Default True)
 
     Returns:
       Dictionary mapping filenames to where they can be accessed online.
     
     """
-    from gdata.docs.service import SUPPORTED_FILETYPES
+    from gdata.docs.data import MIMETYPES
+    uri = ''
+    if folder:
+      folder_feed = self.GetDocList(uri='/feeds/default/private/full/-/folder')
+      folder_entry = None
+      for f in folder_feed.entry:
+        if f.title.text == folder:
+          folder_entry = f
+          break
+      if not folder_entry:
+        create_folder = raw_input('Folder ' + folder + 
+                                  ' not found. Create it? (y/N): ')
+        if create_folder.lower() == 'y':
+          folder_entry = self.create(gdata.docs.data.FOLDER_LABEL, folder)
+      if folder_entry:
+        uri = (gdata.docs.client.FOLDERS_FEED_TEMPLATE % 
+               urllib.quote_plus(folder_entry.resource_id.text))
+    if not uri:
+      uri = gdata.docs.client.DOCLIST_FEED_URI
+    uri += '?convert=' + str(convert).lower()
     url_locs = {}
     for path in paths:
       filename = os.path.basename(path)
-      extension = filename.split('.')[1].upper() 
       try:
-        content_type = SUPPORTED_FILETYPES[extension]
+        content_type = MIMETYPES[filename.split('.')[1].upper()]
       except:
         content_type = 'text/plain'
       print 'Loading ' + path
       try:
-        ms = gdata.MediaSource(file_path=path, content_type=content_type)
-        title = title or filename
-        if extension.lower() in ['csv', 'tsv', 'tab', 'ods', 'xls']:
-          entry = self.UploadSpreadsheet(ms, title)
-        elif extension.lower() in ['ppt', 'pps']:
-          entry = self.UploadPresentation(ms, title)
-        elif extension.lower() in ['doc', 'odt', 'rtf', 'sxw',
-                                   'txt', 'htm', 'html']:
-          entry = self.UploadDocument(ms, title)
-        else:
-          raise Exception('Unexpected extension: ' + extension)
+        entry = self.Upload(path,
+                            title or filename.split('.')[0],
+                            content_type=content_type,
+                            folder_or_uri=uri)
       except Exception, e:
         print 'Failed to upload ' + path
         print e
@@ -253,40 +331,19 @@ class DocsServiceCL(gdata.docs.service.DocsService, util.BaseServiceCL):
   UploadDocs = upload_docs
 
 
-service_class = DocsServiceCL
-
-
-def get_document_type(entry):
-  """Extracts the type of document given DocsEntry is.
-
-  This method returns the type of document the DocsEntry represents. Possible
-  values are document, presentation, spreadsheet, folder, or pdf.
-  This function appears in gdata-2.x.x python client libraries, and is
-  copied here for compatibility with gdata-1.2.4.
-
-  Returns:
-    A string representing the type of document.
-  
-  """
-  data_kind_scheme = 'http://schemas.google.com/g/2005#kind'
-  if entry.category:
-    for category in entry.category:
-      if category.scheme == data_kind_scheme:
-        return category.label
-  else:
-    return None
+service_class = DocsClientCL
 
 
 def get_extension(doctype_label):
   """Return file extension based on document type and preferences file."""
   try:
-    if doctype_label == SPREADSHEET_LABEL:
+    if doctype_label == gdata.docs.data.SPREADSHEET_LABEL:
       return util.config.get(SECTION_HEADER, 'spreadsheet_format')
-    elif doctype_label == DOCUMENT_LABEL:
+    elif doctype_label == gdata.docs.data.DOCUMENT_LABEL:
       return util.config.get(SECTION_HEADER, 'document_format')
-    elif doctype_label == PDF_LABEL:
+    elif doctype_label == gdata.docs.data.PDF_LABEL:
       return 'pdf'
-    elif doctype_label == PRESENTATION_LABEL:
+    elif doctype_label == gdata.docs.data.PRESENTATION_LABEL:
       return util.config.get(SECTION_HEADER, 'presentation_format')
   except ConfigParser.NoOptionError:
     try:
@@ -305,20 +362,18 @@ def get_editor(doctype_label):
   4) If no EDITOR variable, return None.
   
   Keyword arguments:
-    doctype_label: A string representing the type of document to edit.
-  
-  Returns:
-    Editor to use to edit the document.
+    doctype_label: A string representing the type of document to edit. Should
+                   be defined in gdata.docs.data, e.g. SPREADSHEET_LABEL.
   
   """
   try:
-    if doctype_label == SPREADSHEET_LABEL:
+    if doctype_label == gdata.docs.data.SPREADSHEET_LABEL:
       return util.config.get(SECTION_HEADER, 'spreadsheet_editor')
-    elif doctype_label == DOCUMENT_LABEL:
+    elif doctype_label == gdata.docs.data.DOCUMENT_LABEL:
       return util.config.get(SECTION_HEADER, 'document_editor')
-    elif doctype_label == PDF_LABEL:
+    elif doctype_label == gdata.docs.data.PDF_LABEL:
       return util.config.get(SECTION_HEADER, 'pdf_editor')
-    elif doctype_label == PRESENTATION_LABEL:
+    elif doctype_label == gdata.docs.data.PRESENTATION_LABEL:
       return util.config.get(SECTION_HEADER, 'presentation_editor')
   except ConfigParser.NoOptionError:
     try:
@@ -337,9 +392,6 @@ def get_editor(doctype_label):
 #        required
 #===============================================================================
 def _run_get(client, options, args):
-  if not hasattr(client, 'Export'):
-    print 'Downloading documents is not supported for gdata-python-client < 2.0'
-    return
   if not args:
     path = os.getcwd()
   else:
@@ -351,12 +403,12 @@ def _run_get(client, options, args):
   print 'Removing spreadsheets from list of documents...'
   print '(Downloading spreadsheets through the API is currently broken, sorry).'
   entries = [e for e in entries
-             if get_document_type(e) != SPREADSHEET_LABEL]
+             if e.GetDocumentType() != gdata.docs.data.SPREADSHEET_LABEL]
   client.get_docs(path, entries)
 
 
 def _run_list(client, options, args):
-  entries = client.get_doclist(options.title, options.folder)
+  entries = client.get_doclist(options.title)
   if args:
     style_list = args[0].split(',')
   else:
@@ -373,17 +425,14 @@ def _run_upload(client, options, args):
 
 
 def _run_edit(client, options, args):
-  if not hasattr(client, 'Export'):
-    print 'Editing documents is not supported for gdata-python-client < 2.0'
-    return
   doc_entry = client.get_single_doc(options.title, options.folder)
   if not doc_entry:
     print 'No matching documents found!'
     return
-  doc_type = get_document_type(doc_entry)
+  doc_type = doc_entry.GetDocumentType()
   # Spreadsheet exporting is still broken (on the client library side)
   # so we can't let users specify spreadsheets.
-  if doc_type == SPREADSHEET_LABEL:
+  if doc_type == gdata.docs.data.SPREADSHEET_LABEL:
     print 'Sorry, cannot edit or download spreadsheets.'
     return
   else:
