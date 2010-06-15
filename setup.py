b9501166fae42e38854596f04367e79dbaf6c0e2
tom.h.miller <tom.h.miller@03bb9ad7-192f-422b-88de-2d13e6ea63b0>
2010-06-15 18:27:59 +0000

    Fixing release-0.9.1 tag with svn directory updates.

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ setup(name = "googlecl",
         'googlecl.docs',
         'googlecl.picasa',
         'googlecl.youtube'],
-    scripts = ["google"],
+    package_dir = {'googlecl':'src/googlecl'},
+    scripts = ["src/google"],
     long_description = """The Google Data APIs allow programmatic access to
 various Google services.  This package wraps a subset of those APIs into a
 command-line tool that makes it easy to do things like posting to a Blogger
