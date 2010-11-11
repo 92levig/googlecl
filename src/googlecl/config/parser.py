64868e23648b9ff1e7d17b2fc0402fce06e43118
tom.h.miller <tom.h.miller@03bb9ad7-192f-422b-88de-2d13e6ea63b0>
2010-11-11 07:27:27 +0000

    Add from __future__ import for parser.py

--- src/googlecl/config/parser.py
+++ src/googlecl/config/parser.py
@@ -11,8 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Enhanced configuration file parser."""
+
+from __future__ import with_statement
 
 import logging
 import os.path
