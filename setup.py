a46dcf1a1c12c8b0aa3826960dce7dcedaf54259
szymon <szymon@endpoint.com>
2012-11-29 18:55:01 +0000

    Change to 0.9.14.

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ command-line tool that makes it easy to do things like posting to a Blogger
 blog, uploading files to Picasa, or editing a Google Docs file."""
 
 setup(name="googlecl",
-      version="0.9.13",
+      version="0.9.14",
       description="Use (some) Google services from the command line",
       author="Tom H. Miller",
       author_email="tom.h.miller@gmail.com",
