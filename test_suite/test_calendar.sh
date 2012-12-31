dbb42924e49b298b439d9ec554231e8ef55e17c2
szymon <szymon@endpoint.com>
2012-12-31 09:27:33 +0000

    Fixed broken calendar tests. They were failing due to changes which fixed the `--date` bug.

--- test_suite/test_calendar.sh
+++ test_suite/test_calendar.sh
@@ -19,7 +19,6 @@ gdata_directory="$base_directory/gdata_installs"
 output_file="$base_directory/output.txt"
 
 task_title="exampletask"
-task_search_date="2012-11-09"
 task_date="2012-11-10"
 
 touch $output_file
@@ -32,11 +31,11 @@ auth_executed=0
 function check_tasks_number {
 
     should_be \
-        "python google.py calendar list --date $task_search_date -u $auth_username" \
+        "python google.py calendar list --date $task_date -u $auth_username" \
         $1 \
         2 \
         "calendar entry" \
-        "export PYTHONPATH=\"$gdata_directory/gdata-2.0.1/lib/python\" && python ../src/google.py calendar delete --date $task_search_date --title \".*\" -u $auth_username --yes"
+        "export PYTHONPATH=\"$gdata_directory/gdata-2.0.1/lib/python\" && python ../src/google.py calendar delete --date $task_date --title \".*\" -u $auth_username --yes"
         
 }
 
@@ -58,7 +57,7 @@ do
 
   if [[ $auth_executed == "0" ]]; then
     auth_executed=1 
-    python google.py calendar list --date $task_search_date --force-auth -u $auth_username
+    python google.py calendar list --date $task_date --force-auth -u $auth_username
   fi
 
   # Creating new task
@@ -68,7 +67,7 @@ do
   check_tasks_number 1
     
   # Deleting the task
-  python google.py calendar delete --date $task_search_date --title "$task_title" -u $auth_username --yes
+  python google.py calendar delete --date $task_date --title "$task_title" -u $auth_username --yes
 
   check_tasks_number 0
   
